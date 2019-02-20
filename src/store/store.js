6b24cefc499a049fa3f7afb40018b5d9a7d8a18d
MMK CHOWDARY <kondaiah121@gmail.com>
2019-02-20 20:25:00 +0530

    adding logger

--- src/store/store.js
+++ src/store/store.js
@@ -2,4 +2,19 @@ import { createStore, applyMiddleware } from "redux";
 import rootReducer from "../reducers/index";
 import reduxThunk from "redux-thunk";
 
-export const store = createStore(rootReducer, {}, applyMiddleware(reduxThunk));
+const logger = store => {
+  return next => {
+    return action => {
+      console.log("[Middleware dispatching", action);
+      const result = next(action);
+      console.log("[middleware] next state", store.getState());
+      return result;
+    };
+  };
+};
+
+export const store = createStore(
+  rootReducer,
+  {},
+  applyMiddleware(logger, reduxThunk)
+);
